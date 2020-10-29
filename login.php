dfbe2e1c24408b6f4199078ba1ccae79f4be3413
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-29 22:21:57 +0800

    php login2 commit

--- login.php
+++ login.php
@@ -3,7 +3,22 @@
 ?>
 
 
+        <section class="signup-form">
+            <h2>Log In</h2>
 
+            <div class="form-style">
+
+                <form action="signup.inc.php" method="post">
+
+                    <input type="text" name="name" placeholer="Username/Email ...">
+                    <input type="password" name="pwd" placeholer="Password ...">
+
+                    <button type="submit" name="submit">Sign Up</button>
+
+                </form>
+            </div>
+            
+        </section>
 
 
 <?php
