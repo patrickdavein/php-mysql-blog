830bd01adaa4d50eb663f4e424dc9cc0f3363eed
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-29 22:18:56 +0800

    php index commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+  include_once 'header.php';
+?>
+
+        <section class="index-intro">
+            <h1>This is an introduction</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem vel nostrum deleniti qui aspernatur excepturi! Veritatis molestiae ut eius harum!</p>
+        </section>
+
+        <section class="index-categories">
+            <h2>Some Basic Categories</h2>
+            <div class="index-categories-list">
+
+                <div>
+                    <h3>Fun Stuff</h3>
+                </div>
+                <div>
+                    <h3>Serious Stuff</h3>
+                </div>
+                <div>
+                    <h3>Exciting Stuff</h3>
+                </div>
+                <div>
+                    <h3>Boring Stuff</h3>
+                </div>
+            </div>
+        </section>
+
+
+<?php
+  include_once 'footer.php';
+?>
