fedca1ed5781d93ea04d1be0b268e13362a2a96f
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-29 22:22:34 +0800

    php signup2 commit

--- signup.php
+++ signup.php
@@ -3,7 +3,25 @@
 ?>
 
 
+        <section class="signup-form">
+            <h2>Sign Up</h2>
 
+            <div class="form-style">
+
+                <form action="signup.inc.php" method="post">
+
+                    <input type="text" name="name" placeholer="Full name ...">
+                    <input type="text" name="email" placeholer="Email ...">
+                    <input type="text" name="uid" placeholer="Username ...">
+                    <input type="password" name="pwd" placeholer="Password ...">
+                    <input type="password" name="pwd-repeat" placeholer="Repeat Password ...">
+
+                    <button type="submit" name="submit">Sign Up</button>
+
+                </form>
+            </div>
+            
+        </section>
 
 
 <?php
